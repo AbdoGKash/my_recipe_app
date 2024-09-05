12582a142e3c7d8b66601922b185bf6656dd6242
abdo gamal <160290297+AbdoGKash@users.noreply.github.com>
2024-09-05 23:11:16 +0200

    Add necessary packages for project setup

---
+++ my_recipe_app/lib/my_recipe_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyRecipeApp extends StatelessWidget {
+  const MyRecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
