f4e3ea72bc57148a98ae97cf897e5a41d567e948
abdo gamal <160290297+AbdoGKash@users.noreply.github.com>
2024-09-06 01:14:54 +0200

    added some colors and some text styles

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static Color primary = Colors.blue;
+  static Color primary2 = const Color(0xffF5CB58);
+  static Color white = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff000000);
+  static Color darkGrey = const Color(0xff525252);
+  static Color grey = const Color(0xff737477);
+  static Color ligtherGrey = const Color.fromARGB(255, 231, 233, 240);
+  static Color red = const Color.fromARGB(255, 209, 8, 8);
+}
---
+++ lib/core/theming/text_styel.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'colors.dart';
+
+class TextStyles {
+  static TextStyle font25BlackBold = TextStyle(
+    fontSize: 25.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font25WhiteBold = TextStyle(
+    fontSize: 25.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.white,
+  );
+  static TextStyle font25primaryBold = TextStyle(
+    fontSize: 25.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.primary,
+  );
+
+  static TextStyle font35primaryBold = TextStyle(
+    fontSize: 35.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.primary,
+  );
+
+  static TextStyle font13primary300 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w300,
+    color: ColorsManager.primary,
+  );
+  static TextStyle font18primaryBold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.primary,
+  );
+  static TextStyle font13BlackBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.black,
+  );
+  static TextStyle font13GreykW300 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w300,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font14Gray200 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w200,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font14WhiteSemiBold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w800,
+    color: ColorsManager.white,
+  );
+  static TextStyle font16WhiteSemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w800,
+    color: ColorsManager.white,
+  );
+
+  static TextStyle font20WhiteBold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.white,
+  );
+  static TextStyle font16RedBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.red,
+  );
+  static TextStyle font20BlackBold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font20PrimaryBold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.primary,
+  );
+}
