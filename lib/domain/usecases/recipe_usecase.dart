9ca01293d4d0927e2200822c62ece712d0471ad1
abdo gamal <160290297+AbdoGKash@users.noreply.github.com>
2024-09-07 15:54:36 +0200

    add recipe usecase

---
+++ lib/domain/usecases/recipe_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_recipe_app/data/network/api_error_model.dart';
+import 'package:my_recipe_app/domain/entities/recipe.dart';
+import 'package:my_recipe_app/domain/repositories/recipr_repository.dart';
+
+class RecipeUsecase {
+  final IRecipeRepository _recipeRepository;
+  RecipeUsecase(this._recipeRepository);
+  Future<Either<ApiErrorModel, Recipe>> getRecipes() {
+    return _recipeRepository.getRecipes();
+  }
+}
